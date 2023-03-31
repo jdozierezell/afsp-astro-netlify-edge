2347a1d5a7a3ee3c3ebd36b1681e34a583dcc166
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2023-03-31 11:53:18 -0400

    removed everything that is not astro or netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/edge-functions';
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [],
   adapter: netlify(),
   output: 'server'
 });
